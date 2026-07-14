1cd5be6ff03f1a8126c07845d581fe440f948053
Alessio Pellegrini <alessio@pointnet.it>
2026-07-14 22:25:14 +0200

    Sync Requires at least to 7.0 in plugin header

--- pointnet-mailguard.php
+++ pointnet-mailguard.php
@@ -8,7 +8,7 @@
  * Author URI: https://www.pointnet.it/
  * Text Domain: pointnet-mailguard
  * Domain Path: /languages
- * Requires at least: 6.5
+ * Requires at least: 7.0
  * Requires PHP: 8.3
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
